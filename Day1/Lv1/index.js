c184c164f0f53f224e86591f20e3c6bd59f1ded0
damvn14 <damvn14@gmail.com>
2023-09-12 00:42:23 -0700

    days1

---
+++ Day1/Lv1/index.js
@@ -0,0 +1,61 @@
+// const arr = Array()
+// console.log(arr)
+
+// //Lấy mục đầu tiên, mục giữa và mục cuối cùng của mảng
+// const myArray = [1, 2, 3, 4, 5];
+
+// const firstItem = myArray[0];
+
+// const middleIndex = Math.floor(myArray.length / 2);
+// const middleItem = myArray[middleIndex];
+
+
+// const lastIndex = myArray.length - 1;
+// const lastItem = myArray[lastIndex];
+
+// console.log("First item:", firstItem);
+// console.log("Middle item:", middleItem);
+// console.log("Last item:", lastItem);
+
+//
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+// console.log(itCompanies);
+// lấy số lượng công ty
+
+// console.log(itCompanies.length);
+
+// const numberOfCompanies = itCompanies.length;
+// console.log(numberOfCompanies);
+
+// in công ty đầu, giữa và cuối
+
+// const firstCompany = itCompanies[0];
+// console.log(firstCompany);
+
+// const middleIndex = Math.floor(itCompanies.length / 2);
+// const middleCompany = itCompanies[middleIndex];
+// console.log(middleCompany)
+// const lastCompany = itCompanies[itCompanies.length -1];
+// console.log(lastCompany)
+// In ra từng công ty
+// itCompanies.forEach(function(company) {
+//     console.log(company)
+// })
+
+//Thay đổi từng tên công ty thành chữ hoa và in chúng ra
+// itCompanies.forEach(function(company){
+//     const uppercaseCompany = company.toUpperCase();
+//     console.log(uppercaseCompany)
+// })
+
+//In mảng như một câu: Facebook, Google, Microsoft, Apple, IBM,Oracle và Amazon là những công ty CNTT lớn.
+
+// const sentence = itCompanies.join(', ')+ ' là những công ty CNTT lớn.'
+// console.log(sentence)
+
+const companyNameToFind = "Facebook";
+if(itCompanies.includes(companyNameToFind)){
+    console.log(`${companyNameToFind} tồn tại.`);
+} else {
+    console.log(`${companyNameToFind} không tồn tại`)
+}
